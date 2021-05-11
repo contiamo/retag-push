a5cc2e7d5e5fdbcd12375cb2f196b84816ce5c60
Lucas Roesler <roesler.lucas@gmail.com>
2021-05-11 16:08:24 +0200

    feat: Initial retag implementation
    Initial fork from the action template and implementation of the tag+push
    logic.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,63 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import csvparse from 'csv-parse/lib/sync';
+import * as cp from 'child_process'
+
+
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const sourceImage: string = core.getInput('source', {required: true})
+    const targetImages: string[] = await getInputList({name:'target', required:true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    for (let index = 0; index < targetImages.length; index++) {
+      const tag = targetImages[index]
+      await retag(sourceImage, tag)
+      await push(tag)
+    }
 
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+
+const push = async (image: string): Promise<void> => {
+  cp.execSync(`docker push ${image} 2>&1`)
+}
+
+const retag = async (src: string, dst: string): Promise<void> => {
+  core.info(`Retag Image`)
+  cp.execSync(`docker tag ${src} ${dst} 2>&1`)
+}
+
+
+export interface GetInputListProps {
+  name: string,
+  ignoreComma?: boolean,
+  required?: boolean,
+}
+
+export async function getInputList({ name, ignoreComma, required }: GetInputListProps): Promise<string[]> {
+  let res: Array<string> = [];
+
+  const items = core.getInput(name, {required: required});
+
+  for (let output of (await csvparse(items, {
+    columns: false,
+    relaxColumnCount: true,
+    skipLinesWithEmptyValues: true
+  })) as Array<string[]>) {
+    if (output.length == 1) {
+      res.push(output[0]);
+      continue;
+    } else if (!ignoreComma) {
+      res.push(...output);
+      continue;
+    }
+    res.push(output.join(','));
+  }
+
+  return res.filter(item => item).map(pat => pat.trim());
 }
 
 run()
